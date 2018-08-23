66bfd42b957dc0cdebcc48dad4cd8b32f3b80fc6
Robert Polacheck <bobby@MacBook-Pro.local>
2018-08-23 08:58:11 -0500

    Added HTML to the Landing Page and created CSS link

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
 export default function Landing(){
-    return <div>Landing</div>
+    return <div>
+              <title>Jab Fitness</title>
+              <meta charset="utf-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+              <link rel="stylesheet" href="html-template/assets/css/main.css" />
+              <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+            
+            
+
+				<div id="header">
+						<div class="inner">
+							<header>
+								<h1><a href="index.html" id="logo">Helios</a></h1>
+								<hr />
+								<p>Another fine freebie by HTML5 UP</p>
+							</header>
+							<footer>
+								<a href="#banner" class="button circled scrolly">Start</a>
+							</footer>
+						</div>
+				</div>
+              
+            </div>            
 };
