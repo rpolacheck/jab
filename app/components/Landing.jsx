75556013740fbd02925741d01f95e4e024239a4b
Robert Shaw <r.shaw1@yahoo.com>
2018-08-23 21:49:45 -0500

    Initial Landing component

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
 
-export default function Landing(){
-    return <div>
-              <title>Jab Fitness</title>
-              <meta charset="utf-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-              <link rel="stylesheet" href="html-template/assets/css/main.css" />
-              <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
-            
-            
+export default function Landing() {
+	return (
+		<div id="header">
+			{/* Inner */}
+			<div className="inner">
+				<header>
+					<h1><a href="index.html" id="logo">Helios</a></h1>
+					<hr />
+					<p>Another fine freebie by HTML5 UP</p>
+				</header>
+				<footer>
+					<a href="#banner" className="button circled scrolly">Start</a>
+				</footer>
+			</div>
 
-				<div id="header">
-						<div class="inner">
-							<header>
-								<h1><a href="index.html" id="logo">Helios</a></h1>
-								<hr />
-								<p>Another fine freebie by HTML5 UP</p>
-							</header>
-							<footer>
-								<a href="#banner" class="button circled scrolly">Start</a>
-							</footer>
-						</div>
-				</div>
-              
-            </div>            
-};
+			{/* Nav */}
+			<nav id="nav">
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li>
+						<a href="#">Dropdown</a>
+						<ul>
+							<li><a href="#">Lorem ipsum dolor</a></li>
+							<li><a href="#">Magna phasellus</a></li>
+							<li><a href="#">Etiam dolore nisl</a></li>
+							<li>
+								<a href="#">And a submenu &hellip;</a>
+								<ul>
+									<li><a href="#">Lorem ipsum dolor</a></li>
+									<li><a href="#">Phasellus consequat</a></li>
+									<li><a href="#">Magna phasellus</a></li>
+									<li><a href="#">Etiam dolore nisl</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Veroeros feugiat</a></li>
+						</ul>
+					</li>
+					<li><a href="left-sidebar.html">Left Sidebar</a></li>
+					<li><a href="right-sidebar.html">Right Sidebar</a></li>
+					<li><a href="no-sidebar.html">No Sidebar</a></li>
+				</ul>
+			</nav>
+
+		</div>
+	)
+}
