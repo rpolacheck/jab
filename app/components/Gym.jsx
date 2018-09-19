13aa6f5f6351aa5e0877aca49bdfe8298807d453
Robert Polacheck <bobby@MacBook-Pro.local>
2018-09-18 19:51:33 -0500

    Skeleton for Gym

--- app/components/Gym.jsx
+++ app/components/Gym.jsx
@@ -1,5 +1,49 @@
 import React from 'react';
 
 export default function Gym(){
-    return <div>Gym</div>
+    return (
+        <div className="wrapper style1">
+
+					<section id="features" className="container special">
+						<header>
+							<h2>Morbi ullamcorper et varius leo lacus</h2>
+							<p>Ipsum volutpat consectetur orci metus consequat imperdiet duis integer semper magna.</p>
+						</header>
+						<div className="row">
+							<article className="col-4 col-12-mobile special">
+								<a href="#" className="image featured"><img src="images/pic07.jpg" alt="" /></a>
+								<header>
+									<h3><a href="#">Gravida aliquam penatibus</a></h3>
+								</header>
+								<p>
+									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+								</p>
+							</article>
+							<article className="col-4 col-12-mobile special">
+								<a href="#" className="image featured"><img src="images/pic08.jpg" alt="" /></a>
+								<header>
+									<h3><a href="#">Sed quis rhoncus placerat</a></h3>
+								</header>
+								<p>
+									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+								</p>
+							</article>
+							<article className="col-4 col-12-mobile special">
+								<a href="#" className="image featured"><img src="images/pic09.jpg" alt="" /></a>
+								<header>
+									<h3><a href="#">Magna laoreet et aliquam</a></h3>
+								</header>
+								<p>
+									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+								</p>
+							</article>
+						</div>
+					</section>
+
+				</div>
+
+    )
 };
