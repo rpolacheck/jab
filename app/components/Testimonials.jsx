39e2011bfdc19585fb04f3ebe403ebf0135fb47c
Robert Polacheck <bobby@MacBook-Pro.local>
2018-09-18 19:51:45 -0500

    Skeleton for Testimonials

--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -1,5 +1,93 @@
 import React from 'react';
 
-export default function Testimonials(){
-    return <div>Testimonials</div>
+export default function Testimonials() {
+    return (
+
+        <section className="carousel">
+            <div className="reel">
+                
+                <article>
+                    <a href="#" className="image featured"><img src="images/pic01.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Pulvinar sagittis congue</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                </article>
+
+                <article>
+                    <a href="#" className="image featured"><img src="images/pic02.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Fermentum sagittis proin</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                </article>
+
+                <article>
+                    <a href="#" className="image featured"><img src="images/pic03.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Sed quis rhoncus placerat</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                </article>
+
+                <article>
+                    <a href="#" className="image featured"><img src="images/pic04.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Ultrices urna sit lobortis</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                </article>
+
+                <article>
+                    <a href="#" className="image featured"><img src="images/pic05.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Varius magnis sollicitudin</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                </article>
+
+                <article>
+                    <a href="#" className="image featured"><img src="images/pic01.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Pulvinar sagittis congue</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                </article>
+
+                <article>
+                    <a href="#" className="image featured"><img src="images/pic02.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Fermentum sagittis proin</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                </article>
+
+                <article>
+                    <a href="#" className="image featured"><img src="images/pic03.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Sed quis rhoncus placerat</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                </article>
+
+                <article>
+                    <a href="#" className="image featured"><img src="images/pic04.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Ultrices urna sit lobortis</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                </article>
+
+                <article>
+                    <a href="#" className="image featured"><img src="images/pic05.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Varius magnis sollicitudin</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                </article>
+                <span className="forward" ></span>
+                <span className="backward"></span>
+            </div>
+        </section>
+    )
 };
