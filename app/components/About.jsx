be677d6dc8f38d6f5e793516c695ddbcf08c2e67
Robert Polacheck <bobby@MacBook-Pro.local>
2018-09-18 19:50:56 -0500

    Skeleton for About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 export default function About(){
-    return <div>About!</div>
-  };
+    return (
+      <section id="banner">
+      <header>
+        <h2>Hi. You're looking at <strong>Helios</strong>.</h2>
+        <p>
+          A (free) responsive site template by <a href="http://html5up.net">HTML5 UP</a>.
+          Built with HTML5/CSS3 and released under the <a href="http://html5up.net/license">CCA</a> license.
+        </p>
+      </header>
+    </section>
+    
+    
+    )
+};
+
 
   
