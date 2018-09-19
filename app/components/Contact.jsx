5b872aa775be730c77e372e15445bf5c47f307e9
Robert Polacheck <bobby@MacBook-Pro.local>
2018-09-18 19:51:25 -0500

    Skeleton for Contact

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 
 export default function Contact(){
-    return <div>Contact</div>
+    return (
+        <section className="contact">
+            <header>
+                <h3>Nisl turpis nascetur interdum?</h3>
+            </header>
+            <p>Urna nisl non quis interdum mus ornare ridiculus egestas ridiculus lobortis vivamus tempor aliquet.</p>
+            <ul className="icons">
+                <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+                <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="#" className="icon fa-pinterest"><span className="label">Pinterest</span></a></li>
+                <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+                <li><a href="#" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
+            </ul>
+        </section>
+
+
+
+    )
 };
