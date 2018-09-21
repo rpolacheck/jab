3844b8afe37917f83c4e6c80d0ac3a9dcd95f7f5
Robert Polacheck <rpolacheck@gmail.com>
2018-09-21 17:41:52 -0500

    Added Jab pics and text

--- app/components/Gym.jsx
+++ app/components/Gym.jsx
@@ -6,38 +6,35 @@ export default function Gym(){
 
 					<section id="features" className="container special">
 						<header>
-							<h2>Morbi ullamcorper et varius leo lacus</h2>
-							<p>Ipsum volutpat consectetur orci metus consequat imperdiet duis integer semper magna.</p>
+							<h2>Jab Fitness Gym</h2>
+							<p>Our gym offers a variety of equipment, enough to accomodate a full body workout!
+								Our Boxing Timer will keep you on track:  3-minute round & 1 minute rest
+
+							</p>
 						</header>
 						<div className="row">
 							<article className="col-4 col-12-mobile special">
-								<a href="#" className="image featured"><img src="../../assets/images/jabgym1.jpg" alt="" /></a>
-								<header>
-									<h3><a href="#">Gravida aliquam penatibus</a></h3>
-								</header>
+								<a href="#" className="image featured"><img src={require("../../public/images/jabgym1.jpg")} alt="" /></a>
 								<p>
-									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+									Jab offers 10 speedbag stations with varying weights, sizes, and brands.  
+									Feel free to bring your own bag too!
 								</p>
 							</article>
 							<article className="col-4 col-12-mobile special">
-								<a href="#" className="image featured"><img src="images/pic08.jpg" alt="" /></a>
-								<header>
-									<h3><a href="#">Sed quis rhoncus placerat</a></h3>
-								</header>
+								<a href="#" className="image featured"><img src={require("../../public/images/jabgym2.jpg")} alt="" /></a>
 								<p>
-									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+									Jab Fitness has 10 Ringside Leather Heavy Bags ranging from 75 lbs. to 150lbs.
+									Also, 2 Ringside Leather Double End Bags and a wide range of jump ropes - 
+									including speed, leather and beaded ropes.  Jab also offers hand wraps and Ringside Boxing gloves.
 								</p>
 							</article>
 							<article className="col-4 col-12-mobile special">
-								<a href="#" className="image featured"><img src="images/pic09.jpg" alt="" /></a>
-								<header>
-									<h3><a href="#">Magna laoreet et aliquam</a></h3>
-								</header>
+								<a href="#" className="image featured"><img src={require("../../public/images/jabgym3.jpg")} alt="" /></a>
 								<p>
-									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+									Jab also has a Parabody Weight System that includes Bench, Squat, Hack Squat, Lat Pull-Down,
+									Leg Press Machine, Roman Chair/Hyper-Extension, Pull-up and Dip station.  Jab also features 2
+									Stationary Bikes and a number of dumbell freeweights.  If that isn't enough, you can also flip
+									tires and use our Battle Ropes.
 								</p>
 							</article>
 						</div>
