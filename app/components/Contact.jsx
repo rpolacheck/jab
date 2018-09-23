5bbfe576f83dda953c3722f2ee0dc45fcb1e4a7c
Robert Polacheck <rpolacheck@gmail.com>
2018-09-23 13:48:08 -0500

    Added classes to pull font awesome icons

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -8,12 +8,10 @@ export default function Contact(){
             </header>
             <p>Urna nisl non quis interdum mus ornare ridiculus egestas ridiculus lobortis vivamus tempor aliquet.</p>
             <ul className="icons">
-                <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="#" className="icon fa-twitter"><span className="icon circled">Twitter</span></a></li>
+                <li><a href="#" className="icon fa-facebook"><span className="icon circled">Facebook</span></a></li>
+                <li><a href="#" className="icon fa-envelope"><span className="icon circled">Email</span></a></li>
                 <li><a href="#" className="icon fa-pinterest"><span className="label">Pinterest</span></a></li>
-                <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                <li><a href="#" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
             </ul>
         </section>
 
