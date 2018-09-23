54b8e98e490b950ae8d8a2646803115d016bd3d7
Robert Polacheck <rpolacheck@gmail.com>
2018-09-23 15:03:23 -0500

    Added a static map API

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -2,20 +2,30 @@ import React from 'react';
 
 export default function Contact(){
     return (
-        <section id="footer">
-            <header>
-                <h3 className = "contact">Contact Jab</h3>
-            </header>
-            <p>Urna nisl non quis interdum mus ornare ridiculus egestas ridiculus lobortis vivamus tempor aliquet.</p>
-            <ul className="icons">
-                <li><a href="#" className="icon fa-twitter"><span className="icon circled">Twitter</span></a></li>
-                <li><a href="#" className="icon fa-facebook"><span className="icon circled">Facebook</span></a></li>
-                <li><a href="#" className="icon fa-envelope"><span className="icon circled">Email</span></a></li>
-                <li><a href="#" className="icon fa-pinterest"><span className="label">Pinterest</span></a></li>
-            </ul>
-        </section>
+        // Footer
+        <div id="footer">
+            <div class="container">
+                <div class="row">
+                        {/* Contact */}
+                            <section class="contact">
+                                <header>
+                                    <h3>Contact Jab</h3>
+                                </header>
+                                <p>Follow Jab on Twitter and Facebook.  Email Joe with any questions</p>
+                                <ul class="icons">
+                                    <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                                    <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                                    <li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
+                                </ul>
+                            </section>
 
-
-
+                        {/* Map    */}
+                        <div>
+                        <iframe height="450" width="600" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJDXXj1hf3DogRSv9XESj_iTw&key=AIzaSyA4OJ4wDQeFu86oDTaV05cF6LyRkgR5W00" allowfullscreen>
+                        </iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
     )
 };
