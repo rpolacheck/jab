26b0ff78e7ebffea6503b26253cb8798da57e3db
Robert Polacheck <rpolacheck@gmail.com>
2018-09-23 13:47:56 -0500

    Imported font awesome icons

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 //local dependencies
 import App from './app.jsx'
