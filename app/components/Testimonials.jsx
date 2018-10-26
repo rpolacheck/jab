f164cf8ab154c0e9efb3c5b012e6cedba1254cef
Robert Shaw <r.shaw1@yahoo.com>
2018-10-25 20:55:28 -0500

    working on stopping the scroll

--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -1,93 +1,136 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Testimonials() {
+export default class Testimonials extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 0,
+    };
+  }
+
+  reelForward = () => {
+    let { x } = this.state;
+    this.intervalID = setInterval(() => this.setState({ x: x--}), 10)
+  }
+
+  reelBackward = () => {
+    let { x } = this.state;
+    this.intervalID = setInterval(() => this.setState({ x: x++}), 10)
+  }
+
+  stopReel = () => {
+    clearInterval(this.intervalID);
+  }
+
+  render() {
+    const {
+      x,
+    } = this.state;
+
     return (
+      <section className="carousel">
+        <div
+          className="reel"
+          style={{
+            overflow: 'visible',
+            transform: `translate(${x}px, 0px)`,
+          }}
+        >
 
-        <section className="carousel">
-            <div className="reel">
-                
-                <article>
-                    <a href="#" className="image featured"><img src="images/pic01.jpg" alt="" /></a>
-                    <header>
-                        <h3><a href="#">Pulvinar sagittis congue</a></h3>
-                    </header>
-                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-                </article>
+          <article
+          >
+            <a href="#" className="image featured"><img src="images/pic01.jpg" alt="" /></a>
+            <header>
+              <h3><a href="#">Pulvinar sagittis congue</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+          </article>
 
-                <article>
-                    <a href="#" className="image featured"><img src="images/pic02.jpg" alt="" /></a>
-                    <header>
-                        <h3><a href="#">Fermentum sagittis proin</a></h3>
-                    </header>
-                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-                </article>
+          <article>
+            <a href="#" className="image featured"><img src="images/pic02.jpg" alt="" /></a>
+            <header>
+              <h3><a href="#">Fermentum sagittis proin</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+          </article>
 
-                <article>
-                    <a href="#" className="image featured"><img src="images/pic03.jpg" alt="" /></a>
-                    <header>
-                        <h3><a href="#">Sed quis rhoncus placerat</a></h3>
-                    </header>
-                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-                </article>
+          <article>
+            <a href="#" className="image featured"><img src="images/pic03.jpg" alt="" /></a>
+            <header>
+              <h3><a href="#">Sed quis rhoncus placerat</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+          </article>
 
-                <article>
-                    <a href="#" className="image featured"><img src="images/pic04.jpg" alt="" /></a>
-                    <header>
-                        <h3><a href="#">Ultrices urna sit lobortis</a></h3>
-                    </header>
-                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-                </article>
+          <article>
+            <a href="#" className="image featured"><img src="images/pic04.jpg" alt="" /></a>
+            <header>
+              <h3><a href="#">Ultrices urna sit lobortis</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+          </article>
 
-                <article>
-                    <a href="#" className="image featured"><img src="images/pic05.jpg" alt="" /></a>
-                    <header>
-                        <h3><a href="#">Varius magnis sollicitudin</a></h3>
-                    </header>
-                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-                </article>
+          <article>
+            <a href="#" className="image featured"><img src="images/pic05.jpg" alt="" /></a>
+            <header>
+              <h3><a href="#">Varius magnis sollicitudin</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+          </article>
 
-                <article>
-                    <a href="#" className="image featured"><img src="images/pic01.jpg" alt="" /></a>
-                    <header>
-                        <h3><a href="#">Pulvinar sagittis congue</a></h3>
-                    </header>
-                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-                </article>
+          <article>
+            <a href="#" className="image featured"><img src="images/pic01.jpg" alt="" /></a>
+            <header>
+              <h3><a href="#">Pulvinar sagittis congue</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+          </article>
 
-                <article>
-                    <a href="#" className="image featured"><img src="images/pic02.jpg" alt="" /></a>
-                    <header>
-                        <h3><a href="#">Fermentum sagittis proin</a></h3>
-                    </header>
-                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-                </article>
+          <article>
+            <a href="#" className="image featured"><img src="images/pic02.jpg" alt="" /></a>
+            <header>
+              <h3><a href="#">Fermentum sagittis proin</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+          </article>
 
-                <article>
-                    <a href="#" className="image featured"><img src="images/pic03.jpg" alt="" /></a>
-                    <header>
-                        <h3><a href="#">Sed quis rhoncus placerat</a></h3>
-                    </header>
-                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-                </article>
+          <article>
+            <a href="#" className="image featured"><img src="images/pic03.jpg" alt="" /></a>
+            <header>
+              <h3><a href="#">Sed quis rhoncus placerat</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+          </article>
 
-                <article>
-                    <a href="#" className="image featured"><img src="images/pic04.jpg" alt="" /></a>
-                    <header>
-                        <h3><a href="#">Ultrices urna sit lobortis</a></h3>
-                    </header>
-                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-                </article>
+          <article>
+            <a href="#" className="image featured"><img src="images/pic04.jpg" alt="" /></a>
+            <header>
+              <h3><a href="#">Ultrices urna sit lobortis</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+          </article>
 
-                <article>
-                    <a href="#" className="image featured"><img src="images/pic05.jpg" alt="" /></a>
-                    <header>
-                        <h3><a href="#">Varius magnis sollicitudin</a></h3>
-                    </header>
-                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-                </article>
-                <span className="forward" ></span>
-                <span className="backward"></span>
-            </div>
-        </section>
+          <article>
+            <a href="#" className="image featured"><img src="images/pic05.jpg" alt="" /></a>
+            <header>
+              <h3><a href="#">Varius magnis sollicitudin</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+          </article>
+        </div>
+        <span
+          className="forward"
+          onMouseEnter={this.reelForward}
+          onMouseLeave={this.stopReel}
+        />
+        <span
+          className="backward"
+          onMouseEnter={() => {
+            if (x < 0) this.reelBackward()
+          }}
+          onMouseLeave={this.stopReel}
+        />
+      </section>
     )
-};
+  }
+}
